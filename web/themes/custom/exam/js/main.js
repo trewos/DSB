a24cb9d93efc4080e549e3577e15465b8be869a1
trewos <n.samokhvalov@edu.internetdevels.com>
2023-06-28 20:05:20 +0300

    Pages with header and node content  have been stylized and adapted. Fixed errors.Add Search page and fixed bag`s. Finished layout.

--- web/themes/custom/exam/js/main.js
+++ web/themes/custom/exam/js/main.js
@@ -1,5 +1,6 @@
-var inputElement = document.getElementById('edit-submit');
-inputElement.value = '';
+var header = document.querySelector('.region-header')
+var inputElement_1 = header.querySelector('.form-submit');
+inputElement_1.value = '';
 
 document.addEventListener("DOMContentLoaded", function() {
   var blockButton = document.getElementById("block-button");
